8ff03157bf560e18c93d42fbf291505c84151abe
Antério vieira <anteriovieira@gmail.com>
2017-10-15 17:37:45 -0200

    The big jump

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-// const { resolve } = require('path')
+module.exports = function nuxtSassResourcesLoader (options) {
+  if (typeof options === 'string' || Array.isArray(options)) {
+    options = {
+      resources: options
+    }
+  }
 
-export default async function module (moduleOptions) {
-  // const options = Object.assign({}, moduleOptions)
+  const sassResourcesLoader = {
+    loader: 'sass-resources-loader',
+    options: options
+  }
 
-  // this.addPlugin({
-  //   src: resolve(__dirname, '../templates/plugin.js'),
-  //   options
-  // })
+  this.extendBuild((config, { isClient, isServer }) => {
+    const sassLoader = config.module.rules.filter(({test}) => {
+      return ['/\\.sass$/', '/\\.scss$/'].indexOf(test.toString()) !== -1
+    })
+    const vueLoader = config.module.rules.find(({test}) => {
+      return test.toString() === '/\\.vue$/'
+    })
+
+    const loaders = vueLoader.options.loaders;
+
+    Object.keys(loaders).forEach(loader => {
+      if (['sass', 'css'].indexOf(loader) !== -1) {
+        loaders[loader].push(sassResourcesLoader)
+      }
+    })
+
+    Object.keys(sassLoader).forEach(loader => {
+      sassLoader[loader].use.push(sassResourcesLoader)
+    })
+  })
 }
+
+module.exports.meta = require('./package.json')
